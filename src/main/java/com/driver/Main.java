73eaf2ce2b2efa45c73743f282eaef6fb6bae5fa
vasudeva111 <vasudevareddy147@gmail.com>
2023-05-15 20:11:51 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]) {
+        RWOnly obj= new RWOnly();
+
+        obj.setName("vasudeva");
+        String name = obj.getName();
+        System.out.println(name);
+
+
+    }
+
 }
+
+  
